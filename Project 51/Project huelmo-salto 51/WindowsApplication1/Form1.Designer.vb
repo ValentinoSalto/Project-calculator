4cc8f2ba769b95b2a819a894a3719b52b98e0b1b
ValentinoSalto <saltovalentino@gmail.com>
2022-10-17 00:06:16 -0300

    Finalizacion de proyecto

--- Project 51/Project huelmo-salto 51/WindowsApplication1/Form1.Designer.vb
+++ Project 51/Project huelmo-salto 51/WindowsApplication1/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class Calculator
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
@@ -38,156 +38,266 @@ Partial Class Form1
         Me.div = New System.Windows.Forms.Button()
         Me.igual = New System.Windows.Forms.Button()
         Me.TXT1 = New System.Windows.Forms.TextBox()
+        Me.btnborrar = New System.Windows.Forms.Button()
+        Me.btnpunto = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'btn1
         '
-        Me.btn1.Location = New System.Drawing.Point(14, 62)
+        Me.btn1.BackColor = System.Drawing.Color.FromArgb(CType(CType(98, Byte), Integer), CType(CType(101, Byte), Integer), CType(CType(103, Byte), Integer))
+        Me.btn1.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.btn1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(98, Byte), Integer), CType(CType(101, Byte), Integer), CType(CType(103, Byte), Integer))
+        Me.btn1.Font = New System.Drawing.Font("Myanmar Text", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.ForeColor = System.Drawing.Color.White
+        Me.btn1.Location = New System.Drawing.Point(15, 116)
+        Me.btn1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btn1.Name = "btn1"
-        Me.btn1.Size = New System.Drawing.Size(63, 55)
+        Me.btn1.Size = New System.Drawing.Size(114, 100)
         Me.btn1.TabIndex = 0
         Me.btn1.Text = "1"
-        Me.btn1.UseVisualStyleBackColor = True
+        Me.btn1.UseVisualStyleBackColor = False
         '
         'btn2
         '
-        Me.btn2.Location = New System.Drawing.Point(95, 62)
+        Me.btn2.BackColor = System.Drawing.Color.FromArgb(CType(CType(98, Byte), Integer), CType(CType(101, Byte), Integer), CType(CType(103, Byte), Integer))
+        Me.btn2.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.btn2.Font = New System.Drawing.Font("Myanmar Text", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.ForeColor = System.Drawing.Color.White
+        Me.btn2.Location = New System.Drawing.Point(135, 116)
+        Me.btn2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btn2.Name = "btn2"
-        Me.btn2.Size = New System.Drawing.Size(63, 55)
+        Me.btn2.Size = New System.Drawing.Size(114, 100)
         Me.btn2.TabIndex = 1
         Me.btn2.Text = "2"
-        Me.btn2.UseVisualStyleBackColor = True
+        Me.btn2.UseVisualStyleBackColor = False
         '
         'btn3
         '
-        Me.btn3.Location = New System.Drawing.Point(176, 62)
+        Me.btn3.BackColor = System.Drawing.Color.FromArgb(CType(CType(98, Byte), Integer), CType(CType(101, Byte), Integer), CType(CType(103, Byte), Integer))
+        Me.btn3.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.btn3.Font = New System.Drawing.Font("Myanmar Text", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.ForeColor = System.Drawing.Color.White
+        Me.btn3.Location = New System.Drawing.Point(256, 116)
+        Me.btn3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btn3.Name = "btn3"
-        Me.btn3.Size = New System.Drawing.Size(63, 55)
+        Me.btn3.Size = New System.Drawing.Size(114, 100)
         Me.btn3.TabIndex = 2
         Me.btn3.Text = "3"
-        Me.btn3.UseVisualStyleBackColor = True
+        Me.btn3.UseVisualStyleBackColor = False
         '
         'btn4
         '
-        Me.btn4.Location = New System.Drawing.Point(257, 62)
+        Me.btn4.BackColor = System.Drawing.Color.FromArgb(CType(CType(98, Byte), Integer), CType(CType(101, Byte), Integer), CType(CType(103, Byte), Integer))
+        Me.btn4.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.btn4.Font = New System.Drawing.Font("Myanmar Text", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn4.ForeColor = System.Drawing.Color.White
+        Me.btn4.Location = New System.Drawing.Point(378, 116)
+        Me.btn4.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btn4.Name = "btn4"
-        Me.btn4.Size = New System.Drawing.Size(63, 55)
+        Me.btn4.Size = New System.Drawing.Size(114, 100)
         Me.btn4.TabIndex = 3
         Me.btn4.Text = "4"
-        Me.btn4.UseVisualStyleBackColor = True
+        Me.btn4.UseVisualStyleBackColor = False
         '
         'btn5
         '
-        Me.btn5.Location = New System.Drawing.Point(338, 62)
+        Me.btn5.BackColor = System.Drawing.Color.FromArgb(CType(CType(98, Byte), Integer), CType(CType(101, Byte), Integer), CType(CType(103, Byte), Integer))
+        Me.btn5.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.btn5.Font = New System.Drawing.Font("Myanmar Text", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn5.ForeColor = System.Drawing.Color.White
+        Me.btn5.Location = New System.Drawing.Point(500, 116)
+        Me.btn5.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btn5.Name = "btn5"
-        Me.btn5.Size = New System.Drawing.Size(63, 55)
+        Me.btn5.Size = New System.Drawing.Size(114, 100)
         Me.btn5.TabIndex = 4
         Me.btn5.Text = "5"
-        Me.btn5.UseVisualStyleBackColor = True
+        Me.btn5.UseVisualStyleBackColor = False
         '
         'btn6
         '
-        Me.btn6.Location = New System.Drawing.Point(14, 138)
+        Me.btn6.BackColor = System.Drawing.Color.FromArgb(CType(CType(98, Byte), Integer), CType(CType(101, Byte), Integer), CType(CType(103, Byte), Integer))
+        Me.btn6.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.btn6.Font = New System.Drawing.Font("Myanmar Text", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn6.ForeColor = System.Drawing.Color.White
+        Me.btn6.Location = New System.Drawing.Point(14, 222)
+        Me.btn6.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btn6.Name = "btn6"
-        Me.btn6.Size = New System.Drawing.Size(63, 55)
+        Me.btn6.Size = New System.Drawing.Size(114, 100)
         Me.btn6.TabIndex = 5
         Me.btn6.Text = "6"
-        Me.btn6.UseVisualStyleBackColor = True
+        Me.btn6.UseVisualStyleBackColor = False
         '
         'btn7
         '
-        Me.btn7.Location = New System.Drawing.Point(95, 138)
+        Me.btn7.BackColor = System.Drawing.Color.FromArgb(CType(CType(98, Byte), Integer), CType(CType(101, Byte), Integer), CType(CType(103, Byte), Integer))
+        Me.btn7.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.btn7.Font = New System.Drawing.Font("Myanmar Text", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn7.ForeColor = System.Drawing.Color.White
+        Me.btn7.Location = New System.Drawing.Point(135, 222)
+        Me.btn7.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btn7.Name = "btn7"
-        Me.btn7.Size = New System.Drawing.Size(63, 55)
+        Me.btn7.Size = New System.Drawing.Size(114, 100)
         Me.btn7.TabIndex = 6
         Me.btn7.Text = "7"
-        Me.btn7.UseVisualStyleBackColor = True
+        Me.btn7.UseVisualStyleBackColor = False
         '
         'btn8
         '
-        Me.btn8.Location = New System.Drawing.Point(176, 138)
+        Me.btn8.BackColor = System.Drawing.Color.FromArgb(CType(CType(98, Byte), Integer), CType(CType(101, Byte), Integer), CType(CType(103, Byte), Integer))
+        Me.btn8.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.btn8.Font = New System.Drawing.Font("Myanmar Text", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn8.ForeColor = System.Drawing.Color.White
+        Me.btn8.Location = New System.Drawing.Point(256, 222)
+        Me.btn8.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btn8.Name = "btn8"
-        Me.btn8.Size = New System.Drawing.Size(63, 55)
+        Me.btn8.Size = New System.Drawing.Size(114, 100)
         Me.btn8.TabIndex = 7
         Me.btn8.Text = "8"
-        Me.btn8.UseVisualStyleBackColor = True
+        Me.btn8.UseVisualStyleBackColor = False
         '
         'btn9
         '
-        Me.btn9.Location = New System.Drawing.Point(257, 138)
+        Me.btn9.BackColor = System.Drawing.Color.FromArgb(CType(CType(98, Byte), Integer), CType(CType(101, Byte), Integer), CType(CType(103, Byte), Integer))
+        Me.btn9.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.btn9.Font = New System.Drawing.Font("Myanmar Text", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn9.ForeColor = System.Drawing.Color.White
+        Me.btn9.Location = New System.Drawing.Point(378, 222)
+        Me.btn9.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btn9.Name = "btn9"
         Me.btn9.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.btn9.Size = New System.Drawing.Size(63, 55)
+        Me.btn9.Size = New System.Drawing.Size(114, 100)
         Me.btn9.TabIndex = 8
         Me.btn9.Text = "9"
-        Me.btn9.UseVisualStyleBackColor = True
+        Me.btn9.UseVisualStyleBackColor = False
         '
         'btn0
         '
-        Me.btn0.Location = New System.Drawing.Point(338, 138)
+        Me.btn0.BackColor = System.Drawing.Color.FromArgb(CType(CType(98, Byte), Integer), CType(CType(101, Byte), Integer), CType(CType(103, Byte), Integer))
+        Me.btn0.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.btn0.Font = New System.Drawing.Font("Myanmar Text", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn0.ForeColor = System.Drawing.Color.White
+        Me.btn0.Location = New System.Drawing.Point(500, 222)
+        Me.btn0.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btn0.Name = "btn0"
-        Me.btn0.Size = New System.Drawing.Size(63, 55)
+        Me.btn0.Size = New System.Drawing.Size(114, 100)
         Me.btn0.TabIndex = 9
         Me.btn0.Text = "0"
-        Me.btn0.UseVisualStyleBackColor = True
+        Me.btn0.UseVisualStyleBackColor = False
         '
         'sum
         '
-        Me.sum.Location = New System.Drawing.Point(425, 65)
+        Me.sum.BackColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(155, Byte), Integer), CType(CType(86, Byte), Integer))
+        Me.sum.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.sum.Font = New System.Drawing.Font("Myanmar Text", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.sum.ForeColor = System.Drawing.Color.White
+        Me.sum.Location = New System.Drawing.Point(622, 116)
+        Me.sum.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.sum.Name = "sum"
-        Me.sum.Size = New System.Drawing.Size(63, 55)
+        Me.sum.Size = New System.Drawing.Size(114, 100)
         Me.sum.TabIndex = 10
         Me.sum.Text = "+"
-        Me.sum.UseVisualStyleBackColor = True
+        Me.sum.UseVisualStyleBackColor = False
         '
         'res
         '
-        Me.res.Location = New System.Drawing.Point(425, 151)
+        Me.res.BackColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(155, Byte), Integer), CType(CType(86, Byte), Integer))
+        Me.res.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.res.Font = New System.Drawing.Font("Myanmar Text", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.res.ForeColor = System.Drawing.Color.White
+        Me.res.Location = New System.Drawing.Point(622, 222)
+        Me.res.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.res.Name = "res"
-        Me.res.Size = New System.Drawing.Size(63, 55)
+        Me.res.Size = New System.Drawing.Size(114, 100)
         Me.res.TabIndex = 11
         Me.res.Text = "-"
-        Me.res.UseVisualStyleBackColor = True
+        Me.res.UseVisualStyleBackColor = False
         '
         'mul
         '
-        Me.mul.Location = New System.Drawing.Point(425, 239)
+        Me.mul.BackColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(155, Byte), Integer), CType(CType(86, Byte), Integer))
+        Me.mul.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.mul.Font = New System.Drawing.Font("Myanmar Text", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.mul.ForeColor = System.Drawing.Color.White
+        Me.mul.Location = New System.Drawing.Point(622, 330)
+        Me.mul.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.mul.Name = "mul"
-        Me.mul.Size = New System.Drawing.Size(63, 55)
+        Me.mul.Size = New System.Drawing.Size(114, 100)
         Me.mul.TabIndex = 12
         Me.mul.Text = "X"
-        Me.mul.UseVisualStyleBackColor = True
+        Me.mul.UseVisualStyleBackColor = False
         '
         'div
         '
-        Me.div.Location = New System.Drawing.Point(344, 239)
+        Me.div.BackColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(155, Byte), Integer), CType(CType(86, Byte), Integer))
+        Me.div.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.div.Font = New System.Drawing.Font("Myanmar Text", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.div.ForeColor = System.Drawing.Color.White
+        Me.div.Location = New System.Drawing.Point(501, 330)
+        Me.div.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.div.Name = "div"
-        Me.div.Size = New System.Drawing.Size(63, 55)
+        Me.div.Size = New System.Drawing.Size(114, 100)
         Me.div.TabIndex = 13
         Me.div.Text = "/"
-        Me.div.UseVisualStyleBackColor = True
+        Me.div.UseVisualStyleBackColor = False
         '
         'igual
         '
-        Me.igual.Location = New System.Drawing.Point(14, 230)
+        Me.igual.BackColor = System.Drawing.Color.FromArgb(CType(CType(220, Byte), Integer), CType(CType(118, Byte), Integer), CType(CType(51, Byte), Integer))
+        Me.igual.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.igual.Font = New System.Drawing.Font("Myanmar Text", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.igual.ForeColor = System.Drawing.Color.White
+        Me.igual.Location = New System.Drawing.Point(382, 330)
+        Me.igual.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.igual.Name = "igual"
-        Me.igual.Size = New System.Drawing.Size(63, 55)
+        Me.igual.Size = New System.Drawing.Size(114, 100)
         Me.igual.TabIndex = 14
         Me.igual.Text = "="
-        Me.igual.UseVisualStyleBackColor = True
+        Me.igual.UseVisualStyleBackColor = False
         '
         'TXT1
         '
-        Me.TXT1.Location = New System.Drawing.Point(12, 12)
+        Me.TXT1.Location = New System.Drawing.Point(15, 36)
+        Me.TXT1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TXT1.Name = "TXT1"
-        Me.TXT1.Size = New System.Drawing.Size(476, 20)
+        Me.TXT1.Size = New System.Drawing.Size(712, 24)
         Me.TXT1.TabIndex = 15
         '
-        'Form1
+        'btnborrar
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.btnborrar.BackColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(38, Byte), Integer))
+        Me.btnborrar.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.btnborrar.Font = New System.Drawing.Font("Myanmar Text", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnborrar.ForeColor = System.Drawing.Color.White
+        Me.btnborrar.Location = New System.Drawing.Point(14, 331)
+        Me.btnborrar.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnborrar.Name = "btnborrar"
+        Me.btnborrar.Size = New System.Drawing.Size(234, 97)
+        Me.btnborrar.TabIndex = 16
+        Me.btnborrar.Text = "DELETE"
+        Me.btnborrar.UseVisualStyleBackColor = False
+        '
+        'btnpunto
+        '
+        Me.btnpunto.BackColor = System.Drawing.Color.FromArgb(CType(CType(98, Byte), Integer), CType(CType(101, Byte), Integer), CType(CType(103, Byte), Integer))
+        Me.btnpunto.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.btnpunto.Font = New System.Drawing.Font("Myanmar Text", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnpunto.ForeColor = System.Drawing.Color.White
+        Me.btnpunto.Location = New System.Drawing.Point(256, 328)
+        Me.btnpunto.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnpunto.Name = "btnpunto"
+        Me.btnpunto.Size = New System.Drawing.Size(112, 100)
+        Me.btnpunto.TabIndex = 17
+        Me.btnpunto.Text = "."
+        Me.btnpunto.UseVisualStyleBackColor = False
+        '
+        'Calculator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(496, 306)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(38, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(756, 464)
+        Me.Controls.Add(Me.btnpunto)
+        Me.Controls.Add(Me.btnborrar)
         Me.Controls.Add(Me.TXT1)
         Me.Controls.Add(Me.igual)
         Me.Controls.Add(Me.div)
@@ -204,8 +314,10 @@ Partial Class Form1
         Me.Controls.Add(Me.btn3)
         Me.Controls.Add(Me.btn2)
         Me.Controls.Add(Me.btn1)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Calculator"
+        Me.Text = "Calculator"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -226,5 +338,6 @@ Partial Class Form1
     Friend WithEvents div As System.Windows.Forms.Button
     Friend WithEvents igual As System.Windows.Forms.Button
     Friend WithEvents TXT1 As System.Windows.Forms.TextBox
-
+    Friend WithEvents btnborrar As Button
+    Friend WithEvents btnpunto As Button
 End Class
--- Project 51/Project huelmo-salto 51/WindowsApplication1/Form1.vb
+++ Project 51/Project huelmo-salto 51/WindowsApplication1/Form1.vb
@@ -1,66 +1,141 @@
-﻿Public Class Form1
+﻿Public Class Calculator
 
-    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TXT1.TextChanged
+    Dim Operacion As String
+    Dim Num As Nullable(Of Double) = Nothing
+    Dim Num2 As Nullable(Of Double) = Nothing
+    Dim PresionaOperador As Boolean
+
+
+
+
+    Private Sub TXT1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TXT1.TextChanged
 
     End Sub
 
     Private Sub BTN1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn1.Click
+        Concatena()
         TXT1.Text &= "1"
     End Sub
 
     Private Sub BTN2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn2.Click
+        Concatena()
         TXT1.Text &= "2"
     End Sub
 
     Private Sub BTN3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn3.Click
+        Concatena()
         TXT1.Text &= "3"
     End Sub
 
     Private Sub BTN4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn4.Click
+        Concatena()
         TXT1.Text &= "4"
     End Sub
 
     Private Sub BTN5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn5.Click
+        Concatena()
         TXT1.Text &= "5"
     End Sub
 
     Private Sub BTN6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn6.Click
+        Concatena()
         TXT1.Text &= "6"
     End Sub
 
     Private Sub BTN7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn7.Click
+        Concatena()
         TXT1.Text &= "7"
     End Sub
 
     Private Sub BTN8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn8.Click
+        Concatena()
         TXT1.Text &= "8"
     End Sub
 
+
     Private Sub BTN9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn9.Click
+        Concatena()
         TXT1.Text &= "9"
     End Sub
 
     Private Sub BTN10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn0.Click
+        Concatena()
         TXT1.Text &= "0"
     End Sub
 
     Private Sub sum_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles sum.Click
-        TXT1.Text &= " + "
+        HaceOperacion()
+        Operacion = "+"
     End Sub
 
     Private Sub res_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles res.Click
-        TXT1.Text &= " - "
+        HaceOperacion()
+        Operacion = "-"
     End Sub
 
     Private Sub mul_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles mul.Click
-        TXT1.Text &= " X "
+        HaceOperacion()
+        Operacion = "*"
     End Sub
 
     Private Sub div_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles div.Click
-        TXT1.Text &= " / "
+        HaceOperacion()
+        Operacion = "/"
     End Sub
 
     Private Sub igual_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles igual.Click
+        HaceOperacion()
+        Operacion = ""
+    End Sub
+
+    Private Sub Calculator_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnborrar.Click
+
+        Num = Nothing
+        Num2 = Nothing
+        TXT1.Text = ""
+
+    End Sub
+
+    Private Sub btnpunto_Click(sender As Object, e As EventArgs) Handles btnpunto.Click
+        Concatena()
+        If InStr(TXT1.Text, ".", CompareMethod.Text) = 0 Then
+            TXT1.Text &= "."
+        End If
+    End Sub
+
+    Public Sub HaceOperacion()
+        PresionaOperador = True
+        Num2 = Val(TXT1.Text)
+        If Num IsNot Nothing Then
+            Select Case Operacion
+                Case "+"
+                    Num += Num2
+                Case "-"
+                    Num -= Num2
+                Case "*"
+                    Num *= Num2
+                Case "/"
+                    Num /= Num2
+            End Select
+            TXT1.Text = Num
+        Else
+            Num = Num2
+        End If
+
+    End Sub
+
+    Public Sub Concatena()
+        If PresionaOperador = True Then
+            TXT1.Text = ""
+            PresionaOperador = False
+        ElseIf TXT1.Text = "0" Then
+            TXT1.Text = ""
+
+        End If
+    End Sub
+
 End Class
